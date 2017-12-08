ec9df7ef7de02560c3a06f7455cabe8e03282d73
Peter Lee <peter.hbyul.lee@gmail.com>
2017-12-08 11:55:51 -0800

    (wip save)oauth progress

--- server/server.js
+++ server/server.js
@@ -46,27 +46,68 @@ if (env === 'development') {
 const googleClientId = '791754955490-65ovohdld1ug2u8qojpokfuk4sasg4td.apps.googleusercontent.com';
 const googleClientSecret = 'pWjBMyaP1esUQXTC6JUmqAGZ';
 const oauthCallbackURL = '/oauth/google/callback';
+
+let userId;
+let userName;
+let userAvatar;
+let userStocks = [];
+
 passport.use(new GoogleStrategy({
     clientID: googleClientId,
     clientSecret: googleClientSecret,
     callbackURL: oauthCallbackURL
   },
-  function(accessToken, refreshToken, profile, cb) {
-    User.findOrCreate({ googleId: profile.id }, function (err, user) {
-      return cb(err, user);
-    });
+  //this is for sequelize
+  (accessToken, refreshToken, profile, cb) => {
+    // might have to place these variables in the global scope
+    // if create add all of these
+    userId = profile.id;
+    userName = profile.name.givenName;
+    userAvatar = profile.photos[0].value;
+    // connect to pg here
+
+    // INSERT INTO users ("userid", "name", "avatar")
+    // SELECT 'key1', 'value1'
+    // WHERE NOT EXISTS (
+    //     SELECT id, "key", "value"
+    //     FROM node_tag
+    //     WHERE key = 'key1' AND value = 'value1'
+    //     )
+    // returning id, "key", "value"
+
+    const queryString = ''
+
+    // sequelize version
+    // User.findOrCreate({ googleId: profile.id }, (err, user) => {
+    //   //check what user is
+    //   // console.log('in find or create'.toUpperCase())
+    //   // console.log(user)
+    //   // console.log('end in find or create'.toUpperCase())
+    //   return cb(err, user);
+    // });
   }
 ));
+
+passport.serializeUser((user, cb) => {
+  cb(null, user);
+});
+passport.deserializeUser((obj, cb) => {
+  //add something here? get user???
+  cb(null, obj);
+});
 
 app.get('/oauth/google',
   passport.authenticate('google', { scope: ['profile'] }));
 
 app.get('/oauth/google/callback',
   passport.authenticate('google', { failureRedirect: '/login' }),
-  function(req, res) {
+  (req, res) => {
     // Successful authentication, redirect home.
     res.redirect('/');
   });
+
+
+
 
 
 app.listen(SERVER_PORT, () => console.log(`App listening on port ${SERVER_PORT}...`.green));
