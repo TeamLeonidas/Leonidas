e1fc344d444cfbbf5ee1ba2179631d3cf8afae2b
Henry <henryau01@gmail.com>
2017-12-08 21:01:52 -0800

    Cleaned up logs and unused components

--- client/components/StockList/StockList.jsx
+++ client/components/StockList/StockList.jsx
@@ -1,18 +1,10 @@
 import React from 'react';
 import { Collapsible, CollapsibleItem } from 'react-materialize';
-import StockItem from './../StockItem/StockItem.jsx';
 import NewsTab from './../NewsTab/NewsTab.jsx';
 
 
 const StockList = (props) => {
-  // const stockItems = Object.keys(props.stockList).map((symbol) => {
-  //   return <StockItem key={`my-${symbol}`} symbol={symbol.toUpperCase()} stockInfo={props.stockList[symbol]} />;
-  // });
   const stockItems = Object.keys(props.stockList).map((symbol) => {
-    //
-    console.log('this is STOCK LIST')
-    console.log(props.stockList)
-    //
     const { open, close, high, low, volume } = props.stockList[symbol];
     return (
       <CollapsibleItem key={`my-${symbol}`} header={`${symbol.toUpperCase()} open: ${open} close: ${close} high: ${high} low: ${low} volume: ${volume}`} >
