68b4ff291d6b673c0acc2269df506c55f146dc01
Peter Lee <peter.hbyul.lee@gmail.com>
2017-12-08 19:13:50 -0800

    (delete) comments

--- client/components/NewsBox.jsx
+++ client/components/NewsBox.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
-// import { render } from 'react-dom';
 
 const NewsBox = (props) => {
-  console.log(props)
+  // console.log(props)
   return (
     <div className="news-box">
       hidomo im newsbox
--- client/components/NewsTab.jsx
+++ client/components/NewsTab.jsx
@@ -2,20 +2,6 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 import NewsBox from './NewsBox.jsx';
 
-// import { Button, Input, Row } from 'react-materialize';
-
-// const topThree = json.articles.slice(3).map((obj, i) => {
-//   <NewsBox
-//     key = {i}
-//     title = {obj.title}
-//     source = {obj.source.name}
-//     author = {obj.author}
-//     url = {obj.url}
-//     urlToImage = {obj.urlToImage}
-//   />
-// })
-
-
 
 class NewsTab extends Component {
   constructor(props) {
@@ -30,8 +16,6 @@ class NewsTab extends Component {
     })
   }
   render() {
-    console.log('inside NewsTab')
-    console.log(this.state)
     return (
       <div className="news-tab">
         {this.state.articles.map((obj, i) => {
